382dfc37644fab658f3a3680cbf2fbb140930258
davidddfm <davidflores@Davids-MacBook-Pro.local>
2020-07-25 12:37:32 -0500

    commit inicial

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="text-gray-700 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span class="ml-3 text-xl">tailblocks</span>
+          </a>
+          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-gray-900">Inicio</a>
+            <a className="mr-5 hover:text-gray-900">Acerca</a>
+          </nav>
+          <button class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">
+            Comprar
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assets/main.css';
+import './assets/aditional.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
